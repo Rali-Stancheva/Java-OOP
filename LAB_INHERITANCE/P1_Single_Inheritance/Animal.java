e3b1e84bb24be7fbbbecc117199c320d908b11fe
Rali-Stancheva <110462037+Rali-Stancheva@users.noreply.github.com>
2022-08-03 09:48:30 +0300

    Add files via upload

---
+++ LAB_INHERITANCE/P1_Single_Inheritance/Animal.java
@@ -0,0 +1,7 @@
+package OOP.LAB_INHERITANCE.P1_Single_Inheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating…");
+    }
+}
---
+++ LAB_INHERITANCE/P1_Single_Inheritance/Main.java
@@ -0,0 +1,9 @@
+package OOP.LAB_INHERITANCE.P1_Single_Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
