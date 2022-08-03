5dcd02f76046fb8833a903c96a22ef3a28c8c4cd
Rali-Stancheva <110462037+Rali-Stancheva@users.noreply.github.com>
2022-08-03 09:40:57 +0300

    Add files via upload

---
+++ LAB_ABSTRACTION/studentSystem/Main.java
@@ -0,0 +1,16 @@
+package OOP.LAB_ABSTRACTION.studentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentSystem studentSystem = new StudentSystem();
+        String[] input = scanner.nextLine().split("\\s+");
+
+        while (!input[0].equals("Exit")) {
+            studentSystem.executeCommand(input);
+            input = scanner.nextLine().split("\\s+");
+        }
+    }
+}
