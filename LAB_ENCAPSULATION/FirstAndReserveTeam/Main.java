2cb8ab2ebf6f99bd5772a5696226c7eb957b0bf7
Rali-Stancheva <110462037+Rali-Stancheva@users.noreply.github.com>
2022-08-03 09:42:05 +0300

    Add files via upload

---
+++ LAB_ENCAPSULATION/FirstAndReserveTeam/Main.java
@@ -0,0 +1,27 @@
+package OOP.LAB_ENCAPSULATION.FirstAndReserveTeam;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Team team = new Team("Black Eagles");
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split(" ");
+
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person personToAdd = new Person(firstName,lastName,age,salary);
+            team.addPlayer(personToAdd);
+        }
+
+        System.out.printf("First team have %d players\n",team.getFirstTeam().size());
+        System.out.printf("Reserve team have %d players",team.getReserveTeam().size());
+    }
+}
