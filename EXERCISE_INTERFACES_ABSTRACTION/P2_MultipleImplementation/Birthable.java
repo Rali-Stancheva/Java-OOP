c3100cefd69079a801860f3b8f6fb72e19e9250a
Rali-Stancheva <110462037+Rali-Stancheva@users.noreply.github.com>
2022-08-03 10:10:28 +0300

    Add files via upload

---
+++ EXERCISE_INTERFACES_ABSTRACTION/P2_MultipleImplementation/Birthable.java
@@ -0,0 +1,6 @@
+package OOP.EXERCISE_INTERFACES_ABSTRACTION.P2_MultipleImplementation;
+
+public interface Birthable {
+    String getBirthDate();
+
+}
---
+++ EXERCISE_INTERFACES_ABSTRACTION/P2_MultipleImplementation/Person.java
@@ -0,0 +1,6 @@
+package OOP.EXERCISE_INTERFACES_ABSTRACTION.P2_MultipleImplementation;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
