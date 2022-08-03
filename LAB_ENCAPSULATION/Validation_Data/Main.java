2bb9f1ad04b9697e5440d79d0330167fafed7f1c
Rali-Stancheva <110462037+Rali-Stancheva@users.noreply.github.com>
2022-08-03 09:43:51 +0300

    Add files via upload

---
+++ LAB_ENCAPSULATION/Validation_Data/Main.java
@@ -0,0 +1,33 @@
+package OOP.LAB_ENCAPSULATION.Validation_Data;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split(" ");
+
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+            Person person = new Person(firstName,lastName,age,salary);
+            people.add(person);
+        }
+
+        double bonus = Double.parseDouble(scan.nextLine());
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person.toString());
+        }
+
+    }
+}
