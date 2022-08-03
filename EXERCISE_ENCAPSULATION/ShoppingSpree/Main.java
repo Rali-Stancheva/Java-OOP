bea7340eb5745aa4f1b9540e0e002fcf992d8b1d
Rali-Stancheva <110462037+Rali-Stancheva@users.noreply.github.com>
2022-08-03 10:05:37 +0300

    Add files via upload

---
+++ EXERCISE_ENCAPSULATION/ShoppingSpree/Main.java
@@ -0,0 +1,7 @@
+package OOP.EXERCISE_ENCAPSULATION.ShoppingSpree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ EXERCISE_ENCAPSULATION/ShoppingSpree/Person.java
@@ -0,0 +1,46 @@
+package OOP.EXERCISE_ENCAPSULATION.ShoppingSpree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private double money;
+    private List<Product> products;
+
+
+    public Person(String name, double money) {
+        setName(name);
+        setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        if (!name.trim().isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+    }
+
+    public void setMoney(double money) {
+        if (money > 0) {
+            this.money = money;
+        } else {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+    }
+
+    public void buyProduct(Product product) {
+        if (this.money >= product.getCost()) {
+            products.add(product);
+            money -= product.getCost();
+        }else{
+            throw new IllegalArgumentException(this.name + "can't afford Milk");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
