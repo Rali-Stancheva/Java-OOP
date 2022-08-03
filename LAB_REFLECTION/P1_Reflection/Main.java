2a34102aa0d7c14a3d38995b3764cab87aea7e14
Rali-Stancheva <110462037+Rali-Stancheva@users.noreply.github.com>
2022-08-03 09:58:03 +0300

    Add files via upload

---
+++ LAB_REFLECTION/P1_Reflection/Main.java
@@ -0,0 +1,21 @@
+package OOP.LAB_REFLECTION.P1_Reflection;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class reflection = Reflection.class;
+        System.out.println("class " + reflection.getSimpleName());
+
+        Class superClass = reflection.getSuperclass();
+        System.out.println(superClass);
+
+        Class[] interfaces = reflection.getInterfaces();
+        for (Class anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+
+        Object reflectionObject = reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+    }
+}
